a881db9af8c3a8c9b0a7addcf195873415e4c198
Anton <kiyananton@gmail.com>
2022-04-05 15:12:00 +0300

    first commit

---
+++ source/js/main.js
@@ -0,0 +1,13 @@
+// import "./modal";
+// import "./scroll";
+// import "./search";
+
+//import "./education-promo-slider";
+//import "./header-bar";
+import "./categories";
+//import "./advantages";
+//import "./lk-tabs";
+//import "./certificates-slider";
+//import "./filter-slider";
+//import "./program-card";
+//import "./testing";
